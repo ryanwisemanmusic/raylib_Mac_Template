fb0392582c0ffb4e947fc8a500b13c39d942e5a1
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-08-05 15:58:40 -0700

    App V0.01
    I hate it...

--- main.cpp
+++ main.cpp
@@ -8,5 +8,21 @@ using namespace std;
 
 int main()
 {
+    const int screenWidth = 3840;
+    const int screenHeight = 2560;
 
+    InitWindow (screenWidth, screenHeight, "raylib intro");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("You fucking did it!!!!", 800, 810, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+    
 }
